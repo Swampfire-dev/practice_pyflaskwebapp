2998772f4fb8f109b0904546010f2a468d7029dc
Rahul N <rdnagpurkar28@gmail.com>
2026-02-03 23:15:23 +0530

    changes done on app file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello All, this is python flask web application setup. If you reading this.You are on right track. byye</p>"
+    return "<p>Hello All, this is python flask web application setup. If you reading this.You are on right track.Take care everyone!!</p>"
 
 if __name__== '__main__':
     app.run(host='0.0.0.0')
